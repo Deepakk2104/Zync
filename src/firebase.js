840ce4645be843163b56fd2e546153759219bcb9
Deepak Kumar <deepakk12435@gmail.com>
2025-08-25 17:38:31 +0530

    initial

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXCwZH9Ro-9zETTYzPIJ2mJqB2Z7P6oh8Y",
+  authDomain: "zync-web.firebaseapp.com",
+  projectId: "zync-web",
+  storageBucket: "zync-web.firebasestorage.app",
+  messagingSenderId: "958939870606",
+  appId: "1:958939870606:web:50b48c35ef6cf48fe7a0df"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
