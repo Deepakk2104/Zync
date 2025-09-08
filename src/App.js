c977857dd105c679882c799b527567a28dc662e3
Deepak Kumar <deepakk12435@gmail.com>
2025-09-08 17:56:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
