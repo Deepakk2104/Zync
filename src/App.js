4559f56d6c0a113d79fbc890f10b2e6db905671d
Deepak Kumar <deepakk12435@gmail.com>
2025-09-30 23:07:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import Login from "./components/Login";
+import Logi from "./components/Login";
 import Signup from "./components/Signup";
 import Dashboard from "./components/Dashboard";
 import { auth } from "./firebase";
