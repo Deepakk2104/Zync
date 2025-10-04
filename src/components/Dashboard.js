ed1e422a2319a2f80e2f07bd78e416f5bc55454a
Deepak Kumar <deepakk12435@gmail.com>
2025-10-04 15:02:28 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-impor React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import ChatWindow from "./Chatwindow";
 import GroupChat from "./Groupchat";
